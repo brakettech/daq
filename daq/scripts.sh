49296e2fcecef7889e138710bdf909d549313b6a
Rob deCarvalho <robdmc@gmail.com>
2017-09-12 21:26:47 -0400

    working on minimal approach

---
+++ daq/scripts.sh
@@ -0,0 +1,20 @@
+
+# function to put daq tools on the path\n
+daq.init() {
+    export PATH="$HOME/miniconda/bin:$PATH"
+    export PATH="$HOME/miniconda3/bin:$PATH"
+    export PATH="$HOME/anaconda/bin:$PATH"
+    export PATH="$HOME/anaconda3/bin:$PATH"
+    export PATH=/Users/rob/anaconda3/bin:$PATH
+    . activate daq
+}
+
+# function that allows remote ssh access via ngrok
+daq.ngrok() {
+    sudo service ssh --full-restart
+    ngrok tcp 22 --log stdout
+}
+
+daq.update() {
+    pip install -U braket-daq
+}
